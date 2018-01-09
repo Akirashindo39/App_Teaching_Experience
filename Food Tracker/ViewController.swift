7a62e8865093d1ad9452319410998b33fe808cd2
Akira Shindo <as2568@cornell.edu>
2018-01-09 22:36:53 +0900

    This is the file that the students will receive when starting exercise 1.

---
+++ Food Tracker/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Food Tracker
+//
+//  Created by Shindo, Akira on 4/4/16.
+//  Copyright © 2016 Shindo, Akira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var menuLabel: UILabel!
+    @IBOutlet weak var blankLabel: UILabel!
+    
+    
+    
+    
+    
+    
+}
+
