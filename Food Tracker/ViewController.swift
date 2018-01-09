1de6e90ed0c40bc4eea5d03e898e716dabc0dad6
Akira Shindo <as2568@cornell.edu>
2018-01-09 22:42:56 +0900

    This is the file that the students will receive when starting exercise 5.

--- Food Tracker/ViewController.swift
+++ Food Tracker/ViewController.swift
@@ -10,33 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    internal static var sandwichCountVar = 0
+    internal static var hamburgerCountVar = 0
+    internal static var pastaCountVar = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        sandwichStepper.value = Double(ViewController.sandwichCountVar)
+        hamburgerStepper.value = Double(ViewController.hamburgerCountVar)
+        pastaStepper.value = Double(ViewController.pastaCountVar)
+        
+        sandwichCount.text = String(ViewController.sandwichCountVar)
+        hamburgerCount.text = String(ViewController.hamburgerCountVar)
+        pastaCount.text = String(ViewController.pastaCountVar)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBOutlet weak var sandwichCount: UILabel!
+    @IBOutlet weak var sandwichStepper: UIStepper!
+    
+    @IBAction func changeSandwichCount(sender: UIStepper) {
+        sandwichCount.text = Int(sender.value).description
+        ViewController.sandwichCountVar = Int(sender.value)
+    }
+    
+    @IBOutlet weak var hamburgerCount: UILabel!
+    @IBOutlet weak var hamburgerStepper: UIStepper!
+    
+    @IBAction func changeHamburgerCount(sender: UIStepper) {
+        hamburgerCount.text = Int(sender.value).description
+        ViewController.hamburgerCountVar = Int(sender.value)
+    }
+    
+    @IBOutlet weak var pastaCount: UILabel!
+    @IBOutlet weak var pastaStepper: UIStepper!
+    
+    @IBAction func changePastaCount(sender: UIStepper) {
+        pastaCount.text = Int(sender.value).description
+        ViewController.pastaCountVar = Int(sender.value)
+    }
+    
 
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var menuLabel: UILabel!
-    internal static var selectedMenuString = ""
-    
-    
-    @IBAction func sandwichOrdered(sender: UIButton) {
-        ViewController.selectedMenuString = "You chose to get a sandwich!"
-    }
-    
-    
-    @IBAction func hamburgerOrdered(sender: UIButton) {
-        ViewController.selectedMenuString = "You chose to get a hamburger!"
-    }
-    
-    @IBAction func pastaOrdered(sender: UIButton) {
-        ViewController.selectedMenuString = "You chose to get pasta!"
-    }
+
     
     @IBAction func changeColorToRed(sender: UIButton) {
         view.backgroundColor = UIColor.redColor()
